5503e33bb67be013a1bb94e6c52ea4a4b84a2f59
Sosenk <shashankraj3636@gmail.com>
2024-06-26 17:48:48 +0530

    socket.io

--- Server/socket.js
+++ Server/socket.js
@@ -3,7 +3,7 @@ module.exports = {
   init: (httpServer) => {
     io = require('socket.io')(httpServer, {
         cors: {
-          origin: "https://postttitt.netlify.app/",
+          origin: "*",
           methods: ["GET", "POST"],
         },
       });
